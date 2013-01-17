7eeccff73903db6879f07e1f26a95e61adc2e701
Dcabrera007 <danielc00@live.com>
2013-01-16 23:25:45 -0500

    boots

---
+++ app/models/index.rb
@@ -0,0 +1,3 @@
+class Index < ActiveRecord::Base
+  attr_accessible :address, :date, :email, :name, :orderinfo, :phonenumber
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117022638) do
+ActiveRecord::Schema.define(:version => 20130117024746) do
+
+  create_table "indices", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phonenumber"
+    t.string   "address"
+    t.string   "date"
+    t.string   "orderinfo"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "orders", :force => true do |t|
     t.string   "name"
